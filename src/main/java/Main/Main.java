90465611059481aed3fef4adc511f6c2c456ddb0
mr-Fet <mr.f.e.t@mail.ru>
2019-11-28 21:52:29 +0400

    hello

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -44,5 +44,7 @@ public class Main {
                 System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
             con.close();
         }catch(Exception e){ System.out.println(e);}
+
+        System.out.println("Hello");
     }
 }
